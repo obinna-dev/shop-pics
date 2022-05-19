ed50294c1caa396c9c5e9ba93a3a596e7b73982c
obinna-dev <100476375+obinna-dev@users.noreply.github.com>
2022-05-19 10:38:59 +0100

    Setup Proptypes

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from "react"
+import PropTypes from "prop-types"
 import { Context } from "../Context"
 
 function Image({className, img})    {
@@ -24,6 +25,15 @@ function Image({className, img})    {
         {cartIcon}
         </div>
     )
+}
+
+Image.propTypes = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
 }
 
 export default Image
